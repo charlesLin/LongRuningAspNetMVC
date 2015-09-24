035db085b31d9a809bebaeaaf038baa3829e5b9b
charleslin <charles@bankpro.com.tw>
2015-09-24 17:38:00 +0800

    init

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Hosting;
+using System.Web.Mvc;
+using Microsoft.AspNet.SignalR;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            HostingEnvironment.QueueBackgroundWorkItem(t =>
+            {
+                new Processor().LongWork();
+            });
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+
+    public class Processor
+    {
+        public void LongWork()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(1000);
+                var hub = GlobalHost.ConnectionManager.GetHubContext<MyHub>();
+                hub.Clients.All.report("step " + i.ToString());
+            }
+
+        }
+    }
+}
---
+++ WebApplication1/Models/MyHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace WebApplication1.Models
+{
+    public class MyHub : Hub
+    {
+        public void Report(string message)
+        {
+            Clients.All.report(message);
+        }
+    }
+}
